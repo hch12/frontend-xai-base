ba02cb605ddbdd29b3cd0e0cb5e067d8d5b53794
documentary <documentary@qq.com>
2024-11-27 17:52:53 +0800

    新修改

--- src/view/data_analysis/KnowledgeKernels.vue
+++ src/view/data_analysis/KnowledgeKernels.vue
@@ -1,122 +1,135 @@
 <!-- KnowledgeKernels.vue -->
 <template>
-    <div>
-      <div class="centered-content">
-        <h1 style="font-size: 35px; font-family: Gabriola">添加领域特征</h1>
-      </div>
-      <br>
-      <br>
-      <el-button type="primary" @click="openDialog" style="float: right">添加核特征</el-button>
-      <br />
-      <h3>已添加的组合</h3>
-      <ul style="line-height: 3; font-size: 14px;">
-        <li v-for="(combination, index) in addedCombinations" :key="index">
-          第 {{ index + 1 }} 组: {{ getCombinationLabels(combination).join(' + ') }}
-          <el-button type="danger" size="mini" @click="removeCombination(index)">删除</el-button>
-        </li>
-      </ul>
-      <div>
-        <el-dialog title="添加" :visible.sync="dialogVisible" :modal="false">
-          <el-checkbox-group v-model="checkList">
-            <el-checkbox
-              v-for="(item, index) in options"
-              :key="index"
-              :label="index"
-            >
-              {{ index }}: {{ item }}
-            </el-checkbox>
-          </el-checkbox-group>
-          <br />
-          <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="submit">确 定</el-button>
-        </el-dialog>
-      </div>
-      <br>
-      <br>
-      <el-button type="primary" @click="GetKnowledgeKernel">获取领域核特征</el-button>
+  <div>
+    <div class="centered-content">
+      <h1>添加领域特征</h1>
     </div>
+    <br>
+    <br>
+    <el-button type="primary" @click="openDialog" style="float: right">添加核特征</el-button>
+    <br />
+    <h3>已添加的组合</h3>
+    <ul style="line-height: 3; font-size: 14px;">
+      <li v-for="(combination, index) in addedCombinations" :key="index">
+        第 {{ index + 1 }} 组: {{ getCombinationLabels(combination).join(' + ') }}
+        <el-button type="danger" size="mini" @click="removeCombination(index)">删除</el-button>
+      </li>
+    </ul>
+    <div>
+      <el-dialog title="添加" :visible.sync="dialogVisible" :modal="false">
+        <el-checkbox-group v-model="checkList">
+          <el-checkbox
+            v-for="(item, index) in options"
+            :key="index"
+            :label="index"
+          >
+            {{ index }}: {{ item }}
+          </el-checkbox>
+        </el-checkbox-group>
+        <br />
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </el-dialog>
+    </div>
+    <br>
+    <br>
+    <el-button type="primary" @click="GetKnowledgeKernel">获取领域核特征</el-button>
+    <div >
+      <h2 display="false">领域核特征展示</h2>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="GPR领域核特征" name="first">GPR领域核特征</el-tab-pane>
+        <el-tab-pane label="MLR领域核特征" name="second">MLR领域核特征</el-tab-pane>
+        <el-tab-pane label="KNN领域核特征" name="third">KNN领域核特征</el-tab-pane>
+        <el-tab-pane label="SVM领域核特征" name="fourth">SVM领域核特征</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+
 </template>
 
 <script>
 export default {
-  // props:['dialogVisible','checkList','options','addedCombinations','addedValues'],
   name: "KnowledgeKernels",
   data(){
     return{
-    //领域特征变量
-    dialogVisible: false,
-    checkList: [], // 保存用户选择的序号
-    options: [], // 数组中的实际值
-    addedCombinations: [], // 保存添加的组合
-    addedValues: [], // 保存添加的值
+      //领域特征变量
+      dialogVisible: false,
+      checkList: [], // 保存用户选择的序号
+      options: [], // 数组中的实际值
+      addedCombinations: [], // 保存添加的组合
+      addedValues: [], // 保存添加的值
     }
-},
+  },
   mounted() {
     this.GetSign()
   },
-methods: {
-  // 领域特征函数
-  GetKnowledgeKernel() {
-    let params = {
-      V: this.addedCombinations,
-    };
-    this.$axios.post('/user/training', params).then(res => {
-      console.log(params.V);
-      if (res.data.code === 1) {
-        this.$notify({
-          title: '提示',
-          message: '领域核特征表已更新',
-          type: 'success'
-        });
-      } else {
-        this.$notify({
-          title: '警告',
-          message: '无法获取数据',
-          type: 'warning'
-        });
-      }
-    });
-  },
+  methods: {
+    // 领域特征函数
+    GetKnowledgeKernel() {
+      let params = {
+        V: this.addedCombinations,
+      };
+      this.$axios.post('/user/training', params).then(res => {
+        console.log(params.V);
+        if (res.data.code === 1) {
+          this.$notify({
+            title: '提示',
+            message: '领域核特征表已更新',
+            type: 'success'
+          });
+        } else {
+          this.$notify({
+            title: '警告',
+            message: '无法获取数据',
+            type: 'warning'
+          });
+        }
+      });
+    },
 
-  addCombination() {
-    if (this.checkList.length > 0) {
-      this.addedCombinations.push([...this.checkList]); // 保存当前选择
-      this.checkList = []; // 清空当前选择
-    } else {
-      this.$message.warning("请至少选择一个组合");
-    }
-  },
-  getCombinationLabels(combination) {
-    return combination.map((index) => this.options[index]); // 根据序号获取组合项的名称
-  },
-  openDialog() {
-    this.dialogVisible = true;
-  },
-  submit() {
-    this.addCombination();
-    this.dialogVisible = false; // 提交后关闭对话框
-  },
-  removeCombination(index) {
-    this.addedCombinations.splice(index, 1); // 删除指定组合
-  },
-  GetSign() {
-    this.$axios.post('/user/getSign').then(res => {
-      if (res.data.code === 1) {
-        const list = JSON.parse(res.data.data);
-        this.options = list.result.map(item => item.name);
-        this.options.pop();
+    addCombination() {
+      if (this.checkList.length > 0) {
+        this.addedCombinations.push([...this.checkList]); // 保存当前选择
+        this.checkList = []; // 清空当前选择
       } else {
-        this.$notify({
-          title: '警告',
-          message: '无法获取数据',
-          type: 'warning'
-        });
+        this.$message.warning("请至少选择一个组合");
       }
-    })
+    },
+    getCombinationLabels(combination) {
+      return combination.map((index) => this.options[index]); // 根据序号获取组合项的名称
+    },
+    openDialog() {
+      this.dialogVisible = true;
+    },
+    submit() {
+      this.addCombination();
+      this.dialogVisible = false; // 提交后关闭对话框
+    },
+    removeCombination(index) {
+      this.addedCombinations.splice(index, 1); // 删除指定组合
+    },
+    GetSign() {
+      this.$axios.post('/user/getSign').then(res => {
+        if (res.data.code === 1) {
+          const list = JSON.parse(res.data.data);
+          this.options = list.result.map(item => item.name);
+          this.options.pop();
+        } else {
+          this.$notify({
+            title: '警告',
+            message: '无法获取数据',
+            type: 'warning'
+          });
+        }
+      })
+    },
   },
-},
 };
 </script>
 <style scoped>
-
+h1,
+h2 {
+  text-align: center;
+  color: #333;
+}
 </style>
--- src/view/function/Steps.vue
+++ src/view/function/Steps.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>属性分而治之平台</h1>
+    <!-- 主步骤 -->
     <div class="steps">
       <el-steps :active="active1" finish-status="success">
         <el-step title="领域核特征识别"></el-step>
@@ -10,21 +11,87 @@
       </el-steps>
     </div>
 
-    <!-- 子步骤 -->
-    <div v-if="active1 === 0">
+    <!-- 子步骤 1：领域核特征识别 -->
+    <div v-if="active1 === 0" class="sub-steps">
       <el-steps :active="active2" finish-status="success" simple>
         <el-step title="数据集上传"></el-step>
         <el-step title="输入领域核特征"></el-step>
       </el-steps>
-      <br>
-      <div v-if="active2===0">
+      <div v-if="active2 === 0">
         <DataUpload />
       </div>
-      <div v-if="active2===1">
+      <div v-if="active2 === 1">
         <KnowledgeKernels />
       </div>
-      <br>
     </div>
+
+    <!-- 子步骤 2：数据核特征识别 -->
+    <div v-if="active1 === 1" class="sub-steps">
+      <el-steps :active="active2" align-center finish-status="success">
+        <el-step title="构建特征决策表"></el-step>
+        <el-step title="特征决策表聚类"></el-step>
+        <el-step title="最简决策表构建"></el-step>
+        <el-step title="决策表求核"></el-step>
+        <el-step title="正数据核特征识别"></el-step>
+      </el-steps>
+      <div v-if="active2 === 0">
+        <p>构建特征决策表</p>
+      </div>
+      <div v-if="active2 === 1">
+        <p>特征决策表聚类</p>
+      </div>
+      <div v-if="active2 === 2">
+        <p>最简决策表构建</p>
+      </div>
+      <div v-if="active2 === 3">
+        <p>决策表求核</p>
+      </div>
+      <div v-if="active2 === 4">
+        <p>正数据核特征识别</p>
+      </div>
+    </div>
+
+    <!-- 子步骤 3：多方面构效关系分析 -->
+    <div v-if="active1 === 3" class="sub-steps">
+      <el-row :gutter="20" class="navigation">
+        <el-col :span="5">
+          <el-menu
+            default-active="1-1"
+            class="el-menu-vertical-demo narrow-menu"
+            @select="handleSelect"
+          >
+            <el-submenu index="1">
+              <template slot="title">基于 MLR 的材料构效关系提取</template>
+              <el-menu-item index="1-1">可解释方法</el-menu-item>
+              <el-menu-item index="1-2">分析步骤</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">基于 KNN 的材料构效关系提取</template>
+              <el-menu-item index="2-1">可解释方法</el-menu-item>
+              <el-menu-item index="2-2">分析步骤</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">基于 SVR 的材料构效关系提取</template>
+              <el-menu-item index="3-1">可解释方法</el-menu-item>
+              <el-menu-item index="3-2">分析步骤</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">基于 GPR 的材料构效关系提取</template>
+              <el-menu-item index="4-1">可解释方法</el-menu-item>
+              <el-menu-item index="4-2">分析步骤</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+
+        <!-- 右侧内容区域 -->
+        <el-col :span="19">
+          <el-card shadow="always">
+            <component :is="currentComponent" />
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+
 
     <!-- 按钮 -->
     <div class="button-group">
@@ -37,14 +104,15 @@
 <script>
 import DataUpload from "../data_analysis/DataUpload";
 import KnowledgeKernels from "../data_analysis/KnowledgeKernels";
-import FeatureTable from "./FeatureTable";
+
 export default {
-  components: { KnowledgeKernels, DataUpload},
+  components: { KnowledgeKernels, DataUpload },
   data() {
     return {
       active1: 0, // 主步骤索引
       active2: 0, // 子步骤索引
       currentStep: 0, // 当前全局步数
+      currentComponent: "MLR"
     };
   },
   methods: {
@@ -52,13 +120,19 @@ export default {
       if (this.currentStep < 2) {
         this.active1 = 0;
         this.active2 = this.currentStep;
+      } else if (this.currentStep < 7) {
+        this.active1 = 1;
+        this.active2 = this.currentStep - 2;
+      } else if (this.currentStep < 8) {
+        this.active1 = 2;
+        this.active2 = this.currentStep - 7;
       } else {
-        this.active1 = this.currentStep - 2;
-        this.active2 = 0;
+        this.active1 = 3;
+        this.active2 = this.currentStep - 8;
       }
     },
     add() {
-      if (this.currentStep < 5) {
+      if (this.currentStep < 10) {
         this.currentStep++;
         this.updateSteps();
       }
@@ -68,6 +142,12 @@ export default {
         this.currentStep--;
         this.updateSteps();
       }
+    },
+    handleSelect(index) {
+      const componentsMap = {
+        "1-1": "MLR",
+      };
+      this.currentComponent = componentsMap[index] || "MLR"; // 根据索引动态加载组件
     },
   },
 };
@@ -82,5 +162,35 @@ export default {
   border: 1px solid #ddd;
   border-radius: 8px;
   font-family: Arial, sans-serif;
+}
+.sub-steps {
+  margin-top: 20px;
+}
+
+.navigation .narrow-menu {
+  border: 1px solid #ddd;
+  background: #f9f9f9;
+  border-radius: 8px;
+}
+
+.el-menu-item {
+  font-size: 14px;
+}
+
+.el-card {
+  font-size: 16px;
+  padding: 20px;
+}
+
+.el-card h2 {
+  font-size: 18px;
+  color: #e74c3c;
+  margin-bottom: 10px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
 }
 </style>
